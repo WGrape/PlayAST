c8efe8b9a425c94441a178a7403409aca16127b9
Lvsi-China <15239928381@163.com>
2019-07-20 18:58:23 +0800

    开发中...

--- sql/jsCompiler.js
+++ sql/jsCompiler.js
@@ -52,21 +52,88 @@
 
                     work() {
 
+                        // 第 1 阶段, 使用符号表(删减版), 只要是存在于符号表中的字符, 如果其前/后没有空格的话, 加上空格
+                        this.clear();
+
+                        // 第 2 阶段, 把清洁后的 SQL 切词
+                        this.demarcate();
+
+                        // 第 3 阶段, 生成 token 表
+                        this.createTokenTable();
+
+                        return true;
                     },
 
-                    // 切词
-                    demarcate(sql) {
+                    clear() {
+
+                        let sql = SQLCompiler.prototype.tool.globalVariableContainer.sql.toLowerCase();
+
+                        let breakpoint_obj = SQLCompiler.prototype.tool.constContainer.referenceTable.symbolTable;
+                        delete breakpoint_obj['*']; // delete breakpoint_arr['.'];
+                        delete breakpoint_obj['"'];
+                        delete breakpoint_obj["'"];
+                        let breakpoint_arr = SQLCompiler.prototype.tool.objectPropertyToArray(breakpoint_obj);
+
+                        SQLCompiler.prototype.tool.globalVariableContainer.sql_cleared = SQLCompiler.prototype.tool.insertWhiteSpaceInExceptChars(sql, breakpoint_arr);
+                    },
+
+                    demarcate() {
+
+                        let sql = SQLCompiler.prototype.tool.globalVariableContainer.sql_cleared;
+
+                        // 使用空格split, 并过滤掉空元素
+                        SQLCompiler.prototype.tool.globalVariableContainer.sql_lexicon_arr = SQLCompiler.prototype.tool.trimArray(sql.split(" "));
+                    },
+
+                    createTokenTable() {
 
                         let token_arr = [];
+                        let lexicon_arr = SQLCompiler.prototype.tool.globalVariableContainer.sql_lexicon_arr;
 
-                        // 第 1 阶段, 使用符号表(删减版), 只要是存在于符号表中的字符, 如果其前/后没有空格的话, 加上空格
-                        let breakpoint_arr = SQLCompiler.prototype.tool.constContainer.referenceTable.symbolTable;
-                        delete breakpoint_arr['*'];
-                        delete breakpoint_arr['.'];
+                        for (let lexicon of lexicon_arr) {
 
+                            let node = this.generateTokenNode(lexicon);
+                            token_arr.push(node);
+                        }
 
+                        // token 表创建成功
+                        SQLCompiler.prototype.tool.globalVariableContainer.tokenTable = token_arr;
                     },
 
+                    // 根据 lexicon 词汇类型, 生成 token node
+                    generateTokenNode(lexicon) {
+
+                        // {type:"",value:""}
+
+                        // 词汇是关键字
+                        let keywordTable = SQLCompiler.prototype.tool.constContainer.referenceTable.keywordTable;
+                        if (typeof keywordTable.curdStatement[lexicon] !== "undefined" || typeof keywordTable.selectStatement[lexicon] !== "undefined") {
+
+                            return {type: "Keyword", value: lexicon};
+                        }
+
+                        // 词汇是符号
+                        let symbolTable = SQLCompiler.prototype.tool.constContainer.referenceTable.symbolTable;
+                        if (typeof symbolTable[lexicon] !== "undefined") {
+
+                            return {type: "Punctuator", value: lexicon};
+                        }
+
+                        // 词汇是 String
+                        if (lexicon[0] === "'" || lexicon[0] === "\"") {
+
+                            return {type: "String", value: lexicon};
+                        }
+
+                        // 词汇是 Numeric
+                        if (!isNaN(lexicon)) {
+
+                            return {type: "Numeric", value: lexicon};
+                        }
+
+                        // 词汇是 Identifier
+                        return {type: "Identifier", value: lexicon};
+                    }
 
                 },
 
@@ -123,44 +190,49 @@
                     // 关键字表
                     keywordTable: {
 
-                        "Select": 2000,
-                        "Update": 2001,
-                        "Insert": 2002,
-                        "Delete": 2003,
+                        curdStatement: {
+
+                            "select": 2000,
+                            "update": 2001,
+                            "insert": 2002,
+                            "delete": 2003,
+                        },
 
                         selectStatement: {
 
-                            "From": 20000,
-                            "Where": 20001,
-                            "Order": 20002,
-                            "By": 20003,
-                            "Limit": 20004,
-                            "Left": 20005,
-                            "Right": 20006,
-                            "Inner": 20007,
-                            "Outer": 20008,
-                            "FULL": 20009,
-                            "Join": 20010,
-                            "On": 20011,
-                            "Group": 20012,
-                            "Distinct": 20013,
-                            "Like": 20014,
-                            "Not": 20015,
-                            "Between": 20016,
-                            "And": 20017,
-                            "In": 20018,
+                            "from": 20000,
+                            "where": 20001,
+                            "order": 20002,
+                            "by": 20003,
+                            "limit": 20004,
+                            "left": 20005,
+                            "right": 20006,
+                            "inner": 20007,
+                            "outer": 20008,
+                            "full": 20009,
+                            "join": 20010,
+                            "on": 20011,
+                            "group": 20012,
+                            "distinct": 20013,
+                            "like": 20014,
+                            "not": 20015,
+                            "between": 20016,
+                            "and": 20017,
+                            "in": 20018,
+                            "desc": 20019,
+                            "asc": 20020,
                         },
 
                         updateStatement: {
 
-                            "Set": 20010,
-                            "Where": 20002,
+                            "set": 20010,
+                            "where": 20002,
                         },
 
                         insertStatement: {
 
-                            "Into": 20020,
-                            "Values": 20021,
+                            "into": 20020,
+                            "values": 20021,
                         }
                     },
 
@@ -182,68 +254,69 @@
             globalVariableContainer: {
 
                 sql: "",
-
                 sql_error: false,
                 sql_error_msg: "",
 
-                // 当前的Token表
-                tokenTable: {},
+
+                /**
+                 * lexicalAnalysis 词法分析阶段的产物
+                 */
+                sql_cleared: "", // 清洁后的SQL
+                sql_lexicon_arr: "", // SQL词汇数组
+                tokenTable: [], // 当前的Token表
+
 
                 // 当前的AST
                 ast: {},
             },
 
-            // 在期望的字符前后插入字符
-            insertChInExceptChars(str, except_chars, ch = " ") {
+            // 在期望的字符处插入空白
+            insertWhiteSpaceInExceptChars(str, except_chars) {
 
-                function insertStr(soure, start, newStr) {
-
-                    return soure.slice(0, start) + newStr + soure.slice(start)
-                }
+                // function insertStr(source, start, newStr) { return source.slice(0, start) + newStr + source.slice(start) }
 
                 // https://blog.csdn.net/weixin_42203183/article/details/84257252
-                function replacepos(text,start,stop,replacetext){
+                function replacePos(text, start, stop, replace_text) {
 
-                    mystr = text.substring(0,stop-1)+replacetext+text.substring(stop+1);
-                    return mystr;
+                    return text.substring(0, stop) + replace_text + text.substring(stop + 1);
                 }
 
-                let length = str.length, ch_len = ch.length;
-                let i = 0;
-                for (i = 0; i <= length - 1;) {
+                for (let i = 0; typeof str[i] !== "undefined";) {
 
                     // 发现符号
-                    if (except_chars.indexOf(str[i]) >= 0) {
+                    let index = except_chars.indexOf(str[i]);
+                    if (index >= 0) {
 
-                        // 首位就出现了期望的字符
-                        if (0 === i) {
-
-                            // 在前面插入一个字符
-                            str = insertStr(str, i, ch);
-                            i = 2;
-                        }
-
-                        if (i > 0 && str[i - 1] !== " ") {
-
-                            // 在前面插入一个字符
-                            str = insertStr(str, i, ch);
-                            i = i + ch_len * 2;
-                        }
-
-                        if (str[i + 1] !== " ") {
-
-                            // 在后面插入一个字符
-                            str = insertStr(str, i + 1, ch);
-                            i = i + ch_len * 2;
-                        }
-
+                        str = replacePos(str, i, i, " " + except_chars[index] + " ");
+                        i = i + 3;
                         continue;
                     }
-
                     ++i;
                 }
 
                 return str;
+            },
+
+            // 过滤数组空元素
+            trimArray(arr) {
+
+                return arr.filter((item) => item.length > 0 && item.trim() !== "");
+            },
+
+            // 对象转数组
+            objectPropertyToArray(obj) {
+
+                let arr = [];
+
+                for (let property in obj) {
+
+                    if (obj.hasOwnProperty(property)) {
+
+                        arr.push(property); //属性
+                    }
+                }
+
+                return arr;
             }
         }
     };
@@ -254,9 +327,34 @@
 
             return $(this).each(function () {
 
-                return (new SQLCompiler(sql)).init();
+                (new SQLCompiler(sql)).init();
             });
+        },
+
+        SQLCompilerDebug: {
+
+            steps: {
+
+                lexicalAnalysis: {
+
+                    getSQLCleared() {
+
+                        return SQLCompiler.prototype.tool.globalVariableContainer.sql_cleared;
+                    },
+
+                    getSQLLexiconArr() {
+
+                        return SQLCompiler.prototype.tool.globalVariableContainer.sql_lexicon_arr;
+                    },
+
+                    getTokenTable() {
+
+                        return SQLCompiler.prototype.tool.globalVariableContainer.tokenTable;
+                    },
+                }
+            },
         }
+
     });
 
 })();
